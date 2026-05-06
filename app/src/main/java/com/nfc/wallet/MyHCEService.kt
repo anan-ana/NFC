a2b4d93a65182a74b3b591984d95518e7dc3519c
anan-ana <128274201+anan-ana@users.noreply.github.com>
2026-05-06 07:56:33 +0000

    V39

--- app/src/main/java/com/nfc/wallet/MyHCEService.kt
+++ app/src/main/java/com/nfc/wallet/MyHCEService.kt
@@ -5,14 +5,10 @@ import android.os.Bundle
 import android.util.Log
 
 class MyHCEService : HostApduService() {
-
     override fun processCommandApdu(commandApdu: ByteArray?, extras: Bundle?): ByteArray {
-        Log.d("HCE", "收到 APDU 指令")
-        // 回傳成功狀態碼 90 00
         return byteArrayOf(0x90.toByte(), 0x00.toByte())
     }
-
     override fun onDeactivated(reason: Int) {
-        Log.d("HCE", "服務已中斷: $reason")
+        Log.d("HCE", "Deactivated: $reason")
     }
 }
